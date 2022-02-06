16c445dc072c95b4ded9045d7ff4f7eb54435e45
jax2net <jacob@jax2.net>
2022-02-05 22:46:07 -0800

    1.2 and 1.3

--- ch1/1.1.scm
+++ ch1/1.1.scm
@@ -11,3 +11,15 @@
 
 
 ;; 1.2
+(/
+ (+ 5
+    4
+    (- 2 (- 3
+          (+ 6 (/ 1 3)))))
+ (* 3
+    (- 6 2)
+    (- 2 7)))
+
+;; 1.3
+(define (square x) (* x x))
+(define (joe a b c) (- (+ (square a) (square b) (square c)) (square (min a b c))))
